1ad0a1c2168417b8a70d5d12ec23049060ed1a6f
ironhopperz <49368570+ironhopperz@users.noreply.github.com>
2025-07-23 13:32:39 +0900

    chore: comment out unnecessary code(`calculate_map`) (#12)

--- tools/train.py
+++ tools/train.py
@@ -63,8 +63,8 @@ def main():
 
         # mAP는 매 에폭마다 계산하면 시간이 오래 걸리므로,
         # 마지막 에폭이나 특정 주기로만 계산할 수 있습니다.
-        if (epoch + 1) % 5 == 0 or epoch == cfg.NUM_EPOCHS - 1:
-            map_results = calculate_map(...)
+        # if (epoch + 1) % 5 == 0 or epoch == cfg.NUM_EPOCHS - 1:
+        #     map_results = calculate_map(...)
 
         lr_scheduler.step()
 
