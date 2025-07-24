fba646065c7d643e9be9e89dd2d862e813903343
glorybell1984 <glorybell1984@gmail.com>
2025-07-24 17:33:22 +0900

    feat(transforms):add normalization and random vertical flip (#2)
    * Structure Revise/transform_add_Normalize_change_VerticalFlip
    
    * change_transforms_normalize
    
    ---------
    
    Co-authored-by: JONG SEO LEE <gloribell1984@gmail.com>

--- data/transforms.py
+++ data/transforms.py
@@ -2,10 +2,16 @@
 import torch
 import torchvision.transforms.v2 as T
 
+# "하드코딩 된 이미지 정규화 값" base_config.py에서 가져오는 것이 좋습니다.
+# 하지만 여기서는 예시로 하드코딩된 값을 사용합니다.
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+
 def get_basic_transform():
     transforms = []
     transforms.append(T.ToImage())
     transforms.append(T.ToDtype(torch.float32, scale=True))
+    transforms.append(T.Normalize(mean=MEAN, std=STD)) # 정규화 추가
     return T.Compose(transforms)
 
 
@@ -13,8 +19,10 @@ def get_transform(train: bool):
     transforms = []
     transforms.append(T.ToImage())
     transforms.append(T.ToDtype(torch.float32, scale=True))
+    transforms.append(T.Normalize(mean=MEAN, std=STD))#정규화 추가
     if train:
-        transforms.append(T.RandomHorizontalFlip(0.5))
+        transforms.append(T.RandomVerticalFlip(0.5)) # 수직 뒤집기로 변경
+        
     return T.Compose(transforms)
 
 
