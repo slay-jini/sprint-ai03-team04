beac8e74df08443f1ad61e8aada2e06fe564d424
JONG SEO LEE <gloribell1984@gmail.com>
2025-07-24 14:19:22 +0900

    Structure Revise/transform_add_Normalize_change_VerticalFlip

--- data/transforms.py
+++ data/transforms.py
@@ -2,10 +2,16 @@
 import torch
 import torchvision.transforms.v2 as T
 
+# "하드코딩 된 이미지 정규화 값" base_config.py에서 가져오는 것이 좋습니다.
+# 하지만 여기서는 예시로 하드코딩된 값을 사용합니다.
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+
 def get_basic_transform():
     transforms = []
     transforms.append(T.ToImage())
     transforms.append(T.ToDtype(torch.float32, scale=True))
+    transforms.append(T.Normalize(mean=MEAN, std=STD)) # 정규화 추가
     return T.Compose(transforms)
 
 
@@ -14,7 +20,8 @@ def get_transform(train: bool):
     transforms.append(T.ToImage())
     transforms.append(T.ToDtype(torch.float32, scale=True))
     if train:
-        transforms.append(T.RandomHorizontalFlip(0.5))
+        transforms.append(T.RandomVerticalFlip(0.5)) # 수직 뒤집기로 변경
+        transforms.append(T.Normalize(mean=MEAN, std=STD))#정규화 추가
     return T.Compose(transforms)
 
 
